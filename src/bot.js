d16c378649d42538e1655dbd92700d6ba1b052d8
imon064 <rafaelbastian06@gmail.com>
2026-07-11 16:12:44 +0700

    learning discordjs

---
+++ src/bot.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits, Message } = require('discord.js');  // import GatewayIntentBits too
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('ready', ()=>{
+    console.log(`logged in as ${client.user.tag}`);
+})
+
+client.on('messageCreate',(message)=>{
+    if(message.author.bot === true) return;
+    console.log(`[${message.author.tag}] : ${message.content}`);
+    if (message.content === 'hello'){
+        message.channel.send('hello');
+    }
+})
+
+client.login(process.env.DISCORD_BOT_TUTOR_TOKEN)
+  .catch(err => console.error("Login failed:", err));
